1af79ac30cf30035abef71ffeb18374e3f7fdefe
kdmhorn <kenhorn68@gmail.com>
2016-06-07 13:49:59 -0400

    Add files via upload

---
+++ vm-getHostHBA.ps1
@@ -0,0 +1,26 @@
+﻿Function ConvertWWPNToHex {
+	Param ($strWWPNraw)
+	[String] $strWWPNHex = "{0:x}" -f $strWWPNraw
+	[String] $strWWPNHexFormatted = ""
+	For ($i=0;$i -lt 8;$i++)
+	{
+	    $strWWPNHexFormatted += "{0}:" -f $($strWWPNHex.SubString($i * 2, 2))
+	}
+	$strWWPNHexFormatted = $strWWPNHexFormatted.SubString(0, $strWWPNHexFormatted.Length - 1)
+	return $strWWPNHexFormatted
+}
+
+
+## Change Lines below for list of hosts to git
+$hostList = Get-Cluster RIH-CtxEpicNonProd|Get-VMHost
+
+ForEach ($vmhost in $hostList){
+	$wwpnlist = $vmhost|Get-VMHostHba|Where {$_.type -eq 'FibreChannel'}
+	Write $vmhost.Name
+	ForEach ($wwpn in $wwpnlist){
+		$formattedwwpn = ConvertWWPNToHex $wwpn.PortWorldWideName
+		$writeline=$wwpn.Device + "    " + $formattedwwpn
+		Write $writeline
+		}
+	Write `n`r
+	}
