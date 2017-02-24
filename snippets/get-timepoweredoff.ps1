9da8d8b54fdf2702ea291ff641bf96345c55fb98
kdmhorn <kenhorn68@gmail.com>
2017-02-24 11:49:37 -0500

    Update and rename get-time-poweredoff.ps1 to snippets/get-timepoweredoff.ps1

--- snippets/get-timepoweredoff.ps1
+++ snippets/get-timepoweredoff.ps1
@@ -1,6 +1,6 @@
 ﻿$vmlist = Get-VM | Where {$_.PowerState -eq 'PoweredOff'} 
 ForEach ($vm in $vmlist){
-	$vm | Get-VIEvent -Types Info | Where {$_.fullFormattedMessage -match "Power On"} | `
+	$vm | Get-VIEvent -Types Info | Where {$_.fullFormattedMessage -match "Powered Off"} | `
 	Sort-Object -property createdTime |  select -last 1 | %{
 		Write-Host $_.vm.name $_.createdTime | Out-Default}
 }
