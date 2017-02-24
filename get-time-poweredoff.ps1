f81f07e816a9fd286f0cda2d3e40e62d8bca1101
kdmhorn <kenhorn68@gmail.com>
2017-02-24 07:14:22 -0500

    Add files via upload

---
+++ get-time-poweredoff.ps1
@@ -0,0 +1,6 @@
+﻿$vmlist = Get-VM | Where {$_.PowerState -eq 'PoweredOff'} 
+ForEach ($vm in $vmlist){
+	$vm | Get-VIEvent -Types Info | Where {$_.fullFormattedMessage -match "Power On"} | `
+	Sort-Object -property createdTime |  select -last 1 | %{
+		Write-Host $_.vm.name $_.createdTime | Out-Default}
+}
