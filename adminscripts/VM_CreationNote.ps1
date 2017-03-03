dbffa46b298780743f318a4563585f5ae5777cdb
kdmhorn <kenhorn68@gmail.com>
2017-03-02 20:27:13 -0500

    Update VM_CreationNote.ps1

--- adminscripts/VM_CreationNote.ps1
+++ adminscripts/VM_CreationNote.ps1
@@ -1,16 +1,17 @@
 <# 
 .SYNOPSIS 
-   Patch-Cleanup is a script for automating the cleanup of snapshots created at 
-   Lifespan from the Shavlik patching processes.
+   VM_CreationNote replaces the VM Notes field with information on its creation such as
+   date, time, template used etc.
+   
 .DESCRIPTION
-   Patch-Cleanup is run daily as a scheduled task. The script will scan the vCenter
-   servers configured in variable $vcenterlist for snapshots older than 3 days
-   and named "Protect Patch" and remove the snapshots.
+   VM_CreationNote is run daily as a scheduled task. The script will scan the vCenter
+   servers configured in variable $vcenterlist for VM create or clone events and parse 
+   the event for relevant information for the VM creation.
 
 .NOTES 
    File Name  : VM_CreationNote.ps1 
    Author     : KWH
-   Version    : 1.03
+   Version    : 1.01
    
 .INPUTS
    No inputs required
@@ -27,22 +28,19 @@
    No Parameters
 
 .CHANGE LOG
-    #20160531 - KWH - Updated PowerCLI to use module rather than the pssnapin
-    #20160531 - KWH - Removed LSVCENTR5 and LSVCENTER04 from the server list
-    #20160531 - KWH - Added lsvcbcrvcs01 to the server list
-	#20170301 - KWH - Removed canned VM Initialize script execution in favor of get-module
+    #20170301 - KWH - Removed canned VM Initialize script execution in favor of get-module
 
 #>
 
 #Load VMWare modules
 Get-Module -Listavailable VMWare* | Import-Module
 
-$AdminName = "svcvcoadmin@lsmaster.lifespan.org"
-$credfile = "c:\Scripts\credentials\svcvcoadmin-cred.txt"
+$AdminName = "account@domain.com"
+$credfile = "c:\Scripts\credentials\admin-cred.txt"
 $password = get-content $CredFile | convertto-securestring
 $Cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $AdminName,$password
 
-$vCenterServers = @("lsvcrihvcs01.lifespan.org","lsvccorvcs01.lifespan.org","lsvcrihvcs02.lifespan.org","lsvccorvcs02.lifespan.org","lsvcbcrvcs01.lifespan.org")
+$vCenterServers = @("vcenter1","vcenter2","vcenter3")
 ForEach ($vcenter in $vCenterServers){
 	Connect-VIServer $vcenter  -Credential $Cred -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
 	$TargetVM = $null
