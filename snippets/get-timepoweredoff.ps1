1afe914514440537c906efd9fb6c8f17c383a0a1
kdmhorn <kenhorn68@gmail.com>
2017-03-06 07:41:58 -0500

    Delete get-timepoweredoff.ps1

--- snippets/get-timepoweredoff.ps1
+++
@@ -1,6 +0,0 @@
-﻿$vmlist = Get-VM | Where {$_.PowerState -eq 'PoweredOff'} 
-ForEach ($vm in $vmlist){
-	$vm | Get-VIEvent -Types Info | Where {$_.fullFormattedMessage -match "Powered Off"} | `
-	Sort-Object -property createdTime |  select -last 1 | %{
-		Write-Host $_.vm.name $_.createdTime | Out-Default}
-}
