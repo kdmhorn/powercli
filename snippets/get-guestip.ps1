e8d65fc93c4067c98f6a712eba19745b68900214
kdmhorn <kenhorn68@gmail.com>
2017-03-06 07:42:53 -0500

    Delete get-guestip.ps1

--- snippets/get-guestip.ps1
+++
@@ -1,5 +0,0 @@
-﻿Get-VM |
-Select Name,@{N='IP Address';E={
-    @($_.guest.IPAddress | where{[ipaddress]::Parse($_).AddressFamily -eq 'InterNetwork'}
-    $_.Guest.IpAddress | where{[ipaddress]::Parse($_).AddressFamily -eq 'InterNetworkv6'}) -join ','
-    }}
