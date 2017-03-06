572e508155604afab07e66bf473d5c3766ed2d4b
kdmhorn <kenhorn68@gmail.com>
2017-03-06 09:20:08 -0500

    Delete vm-updateHostProfileFromReference.ps1

--- vm-updateHostProfileFromReference.ps1
+++
@@ -1,9 +0,0 @@
-﻿$vmhpList = Get-VMHostProfile
-ForEach ($vmhp in $vmhpList){
-	write $vmhp.name
-	$Spec = New-Object VMware.Vim.HostProfileHostBasedConfigSpec
-	$Spec.Host = New-Object VMware.Vim.ManagedObjectReference
-	$Spec.Host = $vmhp.ReferenceHost.ExtensionData.MoRef
-	$Spec.useHostProfileEngine = $true
-	$vmhp.ExtensionData.UpdateHostProfile($Spec)
-}
