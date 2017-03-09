183d86627b2adf9a1a46765e1db121daf2a7ad13
kdmhorn <kenhorn68@gmail.com>
2017-03-09 11:25:38 -0500

    Add files via upload

---
+++ snippets/get-vmtoolsversion.ps1
@@ -0,0 +1,3 @@
+﻿#Returns a list of Virtual machines along with the Version on VM tools installed
+
+get-vm | % { get-view $_.id } | select Name, @{ Name="ToolsVersion"; Expression={$_.config.tools.toolsVersion}},@{ Name="ToolStatus"; Expression={$_.Guest.ToolsVersionStatus}}
